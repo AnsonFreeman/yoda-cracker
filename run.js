5d72da373dd9b8d3aa8a082ea715c4f4e0db5aed
Anson <srkid0223@163.com>
2023-03-08 22:22:42 +0800

    update ua

--- run.js
+++ run.js
@@ -151,7 +151,7 @@ server.on('request', (req, res) => {
       'yoda-language': 'zh-CN',
       'Content-Type': 'application/x-www-form-urlencoded',
       'sec-ch-ua-mobile': '?0',
-      'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36',
+      'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36',
       'sec-ch-ua-platform': '"macOS"',
       'Accept': '*/*',
       'Origin': 'https://epassport.meituan.com',
--- tests/login.sh
+++ tests/login.sh
@@ -3,6 +3,6 @@ curl 'https://epassport.meituan.com/api/account/login?service=hotel&bg_source=4&
   -H 'Accept-Language: zh,en;q=0.9,zh-CN;q=0.8,sq;q=0.7' \
   -H 'Content-Type: application/json;charset=UTF-8' \
   -H 'Origin: https://epassport.meituan.com' \
-  -H 'User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36' \
+  -H 'User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36' \
   --data-raw '{"login":"123456","part_key":"","password":"123456","error":"需要滑块验证","success":"","isFetching":false,"loginType":"account","verifyRequestCode":"","verifyResponseCode":"","captchaCode":"","verifyType":3,"captchaToken":"","rohrToken":"eJyVU39v2jAQ/S6W4B8scH44JEhoCk3ZWgotK+3YJlSFYIKXEEe2AxVVv/suATo2rWorWeend3fPlxf7CcmLBeo8oWUaxqhjEEJs6xmjDZOog4wmaToII60g5bSpS9uW47kuwSg64ahBLMfBaC7vA9T5aXgmwR5xZiXzFYg9YxCXzPAh69IZNm1YZdEF1KCV1rnqtFosD5XKhdTNNeO6CLNmJNatMIpEkelWkXHNNywVMc8+KSY3PGLdldAsrVecEoUExrDrmbjlcVbkXS0LVp/HD4eUvS8sZNqtjqxZfs3sw2JzIRKexafnVnQCseo57s2VXqf1SGQadNj7ZOIthMNXAJrzHUTFtBYJy2pWX7IFlyzSD4XkNSvYa5q0VKWlLn1NmR5GpC9D0j/C1aTwAxHYvJ6UNnumA667wP0ftf9C3vsQJR9Gxj/I/QAyK+S9iazXkEXeRHZpWVJaBnt4ah3u3U0m1yOoNIjnYtvEftnVtjFc7WMSmvSxeQgvCSoUXEdA7HLb396fD+Izf9wbnbWZH1g3DS4TZ6x0Hu/i21EvbzS8xdWwHySDHl2q9GY63LnkyhwPN8G4oc/lavdt+ktL+n2gHj8vJ5fTH18acZ9e9zIfPf8Gq80Kvw=="}' \
   --compressed
